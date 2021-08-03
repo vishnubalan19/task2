fb56711f4971e81c39d348d2eca0d1938a20e8c0
vishnubalan19 <vishnu71900@gmail.com>
2021-08-03 12:23:56 +0530

    update 1

--- com/task2/overloadingdemo/OverloadingDemo.java
+++ com/task2/overloadingdemo/OverloadingDemo.java
@@ -2,6 +2,9 @@ package com.task2.overloadingdemo;
 
 public class OverloadingDemo{
     int val1=0,val2=0,val3=0;
+    public String toString(){
+        return val1+" "+val2+" "+val3;
+    }
     OverloadingDemo(){
         val1=1;
         val2=1;
@@ -16,7 +19,5 @@ public class OverloadingDemo{
         this.val1=val1;
         this.val2=val2;
     }
-    public String toString(){
-        return val1+" "+val2+" "+val3;
-    }
+
 }
